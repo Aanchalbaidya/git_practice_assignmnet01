fb0085ebb2d4318182e4db54ada43a5140b48fcf
ujjwalara95 <112754525+ujjwalara95@users.noreply.github.com>
2022-09-22 01:40:34 +0530

    Update prime_problem.js
    it's done...

--- prime_problem.js
+++ prime_problem.js
@@ -5,25 +5,10 @@ let n=10;
  }  else { 
 
  for(let i=2;i<=n/2;i++){
-
-
- for(let i=2;i<n/2;i++){
-
- for(i=2;i<n;i++){
-
-
     if(n%i===0){
         prime=false;
         break;
     }
-}
-
- prime===true?console.log("Yes"):console.log("NO");
-
+ }
  prime===true?console.log("Yes"):console.log("No");
-
- }
-
-
- }
 
