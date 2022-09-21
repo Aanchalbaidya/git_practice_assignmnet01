014a7a7f034db857f9ca484ade0779c14fed232a
ujjwalara95 <112754525+ujjwalara95@users.noreply.github.com>
2022-09-22 01:20:24 +0530

    Update prime_problem.js
    Check kar lo..,.

--- prime_problem.js
+++ prime_problem.js
@@ -1,13 +1,13 @@
 let n=10;
  let prime=true;
  if(n==1){
-    console.log('No');
+    console.log("No);
  }  else { 
- for(i=2;i<n;i++){
-    if(n%i===1){
+ for(i=2;i<=n/2;i++){
+    if(n%i===0){
         prime=false;
         break;
     }
 }
- prime===true?console.log("Yes"):console.log("yes");
+ prime===true?console.log("Yes"):console.log("No");
  }
